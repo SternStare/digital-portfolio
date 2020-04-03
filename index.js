4fa9d8bb8669499c04211b627391452b6b5415e2
SternStare <63106461+SternStare@users.noreply.github.com>
2020-04-03 14:06:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let name;
+name = "Connor";
+let age = "14";
+let school = "Code Nation"
+
+console.log("hi, my name is " + name);
+name = "bob";
+console.log("hi, my name is" + name); 
+let numberOne;
+let numberTwo;
+let total;
+
+console.log("i am " + age + "years old");
+console.log( "I attend " + school);
+
+const addition = (numberOne,numberTwo) => {
+    total = numberOne + numberTwo
+    console.log(total)
+}
+addition (20, 30);
+addition(30, 70);
+addition(40, 15);
